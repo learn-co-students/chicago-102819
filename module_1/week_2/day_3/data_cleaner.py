21a20fd9382b38272233d6de1aa22f235b55a166
Max <jmax.barry@gmail.com>
2019-11-06 13:58:51 -0600

    Update data_cleaner .py

--- module_1/week_2/day_3/data_cleaner.py
+++ module_1/week_2/day_3/data_cleaner.py
@@ -17,11 +17,18 @@ def combing_types(row):
 
 
 def clean_df(df):
+    df['MonthYear'] = pd.to_datetime(df['MonthYear'])
+    df['DateTime'] = pd.to_datetime(df['DateTime'])
+    df['Date of Birth'] = pd.to_datetime(df['Date of Birth'])
+    
+    df['age_at_outcome'] = df['MonthYear'] - df['Date of Birth']
+    df['days_at_outcome'] = df['age_at_outcome'].apply(lambda x: x.days)
     
     df['Name'].fillna('no_name', inplace = True)
     df['combined_outcome'] = df.apply(combing_types, axis = 1)
     df.drop(['Outcome Subtype'], axis =1, inplace = True)
     df.dropna(inplace = True)
+    df.drop('MonthYear', axis = 1, inplace = True)
     
     
     return df
