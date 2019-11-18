f8dbd3a78ae214a64899c278dbc37c5daf896874
Max <jmax.barry@gmail.com>
2019-11-18 07:27:28 -0600

    Add mod_2 day_2 warmup

---
+++ module_2/week_1/day_2/warm_up_day_2.py
@@ -0,0 +1,10 @@
+# Complete the function that accepts a string parameter, and reverses each word
+#in the string. All spaces in the string should be retained.
+#
+# “This is an example!” ==> “sihT si na !elpmaxe”
+# “double  spaces”      ==> “elbuod  secaps”
+
+def topsy_turvy(string_list):
+
+    
+    
